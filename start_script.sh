9958a3f66d3e833044e118ea2bb9c10e2cec7014
Pradeep Mahato <pradeepmahato007@gmail.com>
2020-11-08 03:29:41 -0800

    corrections to start script

--- start_script.sh
+++ start_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# redis-server &
+redis-server &
 cd /home/QMine_miningregex_core/ && make server > /var/log/qmine_c.out &
 cd /home/QMineWebApp/ && pm2 start app.js -l /var/log/qmine_node.out
 frontail /var/log/qmine_c.out /var/log/qmine_node.out
