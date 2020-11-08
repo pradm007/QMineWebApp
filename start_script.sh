50d9924aafe25b976fdcb9069f773c17839bb12e
Pradeep Mahato <pradeepmahato007@gmail.com>
2020-11-08 03:11:54 -0800

    corrections to start script

--- start_script.sh
+++ start_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-redis-server &
+# redis-server &
 cd /home/QMine_miningregex_core/ & make server > /var/log/qmine_c.out &
-cd /home/QMineWebApp/ & pm2 start app.js -l /var/log/qmine_node.out &
+cd /home/QMineWebApp/ & pm2 start app.js -l /var/log/qmine_node.out
 frontail /var/log/qmine_c.out /var/log/qmine_node.out
 
 # now we bring the primary process back into the foreground
 # and leave it there
-fg %1
+# fg %1
+
+# /bin/bash
